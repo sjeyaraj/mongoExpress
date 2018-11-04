bb5f43ef270ef2aaf54e899b7f302c7c4c720f22
Stephen <j_h_stephen@yahoo.com>
2018-11-04 10:57:18 -0600

    projection

--- companies/findCursor.js
+++ companies/findCursor.js
@@ -9,10 +9,13 @@ MongoClient.connect(
     console.log("Successfully connected to MongoDB.");
 
     const query = { category_code: "biotech" };
+    const projection = { name: 1, category_code: 1, _id: 0 };
     const cursor = db.collection("companies").find(query);
+    cursor.project(projection);
 
     cursor.forEach(doc => {
       console.log(doc.name + " is a " + doc.category_code + " company.");
+      console.log(doc);
     }),
       err => {
         assert.equal(err, null);
---
+++ companies/projection.js
@@ -0,0 +1,29 @@
+var MongoClient = require("mongodb").MongoClient,
+  assert = require("assert");
+
+MongoClient.connect(
+  "mongodb://localhost:27017/crunchbase",
+  function(err, db) {
+    assert.equal(err, null);
+    console.log("Successfully connected to MongoDB.");
+
+    var query = { founded_year: 2010 };
+
+    var cursor = db.collection("companies").find(query);
+
+    cursor.project({ name: 1, number_of_employees: 1, _id: 0 });
+
+    cursor.forEach(
+      function(doc) {
+        console.log(
+          doc.name + " has " + doc.number_of_employees + " employees."
+        );
+        console.log(doc);
+      },
+      function(err) {
+        assert.equal(err, null);
+        return db.close();
+      }
+    );
+  }
+);
